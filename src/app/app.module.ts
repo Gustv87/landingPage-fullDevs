2159cbfb52280110d1e1079d25756f7a0f95f2b5
Gustv87 <tavomartinez87@gmail.com>
2020-04-01 21:16:55 -0600

    Se creo el landing page de full devs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeamComponent } from './components/team/team.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    PricingComponent,
+    AboutComponent,
+    TeamComponent,
+    ContactComponent,
+    FooterComponent,
+    NavbarComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
